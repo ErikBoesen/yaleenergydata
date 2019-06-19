1ebc70b71068cf347edea499aa208a44b605ac88
ErikBoesen <me@erikboesen.com>
2019-06-19 19:05:10 -0400

    Add example.py content

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
 import yaleenergydata
+import os
 
-api = yaleenergydata.YaleEnergyData()
+api = yaleenergydata.YaleEnergyData(os.environ['YALE_API_TOKEN'])
+
+print(api.get('data', params={
+    'buildingID': '3600',
+    'rangeStart': '2015-07-01',
+    'rangeEnd': '2015-08-01',
+}))
