c6ce415b86cd92c6d75ca5a7972c18488e3c0510
ErikBoesen <me@erikboesen.com>
2019-06-19 19:05:29 -0400

    Add starting GET logic

--- yaleenergydata/main.py
+++ yaleenergydata/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 
 class ConnectionError(Exception):
@@ -7,21 +8,32 @@ class ConnectionError(Exception):
 
 
 class YaleEnergyData:
-    API_ROOT = 'https://gw.its.yale.edu/soa-gateway/energy/'
+    API_TARGET = 'https://gw.its.yale.edu/soa-gateway/energy/data'
 
     def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def get(self, endpoint: str, params: dict = {}):
+    def get(self, params: dict = {}):
         """
         Make a GET request to the API.
 
         :param endpoint: path to resource desired.
         :param params: dictionary of custom params to add to request.
         """
-        request = requests.get(self.API_ROOT + endpoint)
+        params['apikey'] = self.api_key
+        request = requests.get(self.API_TARGET, params=params)
         if request.ok:
             return request.json()
         else:
             # TODO: Can we be more helpful?
             raise ConnectionError('API request failed.')
+
+    def fetch(self, building_id: str, start_date, end_date):
+        """
+        Build a request to the API and fetch data within a given date range.
+
+        :param building_id: ID of building to get data on. You may wish to use Yale's Building API to find an ID.
+        :param start_date: date to start sampling from. Can be a string or datetime object.
+        :param end_date: date to end sampling at. Formatting is the same as start_date.
+        """
+        self.get(
