07c5b25b8a74da0bc1d09f2b0f2aea2dcea7ca4b
ErikBoesen <me@erikboesen.com>
2019-06-19 20:04:05 -0400

    Write starting date processing code

--- yaleenergydata/main.py
+++ yaleenergydata/main.py
@@ -1,10 +1,10 @@
 import requests
-from datetime import datetime
+import date
 
 
-class ConnectionError(Exception):
-    """Raised when an error occurs in connecting to the API."""
-    pass
+class Building:
+    def __init__(self, raw):
+
 
 
 class YaleEnergyData:
@@ -26,14 +26,43 @@ class YaleEnergyData:
             return request.json()
         else:
             # TODO: Can we be more helpful?
-            raise ConnectionError('API request failed.')
+            raise Exception('API request failed.')
 
-    def fetch(self, building_id: str, start_date, end_date):
+    def stringify_date(self, date) -> str:
+        """
+        Convert a datetime object to a string in the approved format if necessary.
+        """
+        if type(date) in (datetime.datetime, datetime.date):
+            date = date.strftime('%Y-%m-%d')
+        return date
+
+    def fetch(self, building_id: str, start_date, end_date=None):
         """
         Build a request to the API and fetch data within a given date range.
 
         :param building_id: ID of building to get data on. You may wish to use Yale's Building API to find an ID.
-        :param start_date: date to start sampling from. Can be a string or datetime object.
-        :param end_date: date to end sampling at. Formatting is the same as start_date.
+        :param start_date: date to start sampling from. Can be a string or datetime/date object.
+        :param end_date: date to end sampling at. Formatting is the same as start_date. If not specified, today.
         """
-        self.get(
+        start_date = stringify_date(start_date)
+        end_date = stringify_date(end_date || datetime.date.today())
+        raw = self.get({
+            'buildingID': building_id,
+            'rangeStart': start_date,
+            'rangeEnd': end_date,
+        })
+        days = {}
+        for raw_item in raw:
+            date = raw_item['MENUDATE']
+            meal_code = raw_item['MEALCODE']
+            item = Item(raw_item, self)
+            if days.get(date) is None:
+                days[date] = {}
+            if days[date].get(meal_code) is None:
+                days[date][meal_code] = Meal(raw_item, self)
+            days[date][meal_code].items.append(item)
+        meals = []
+        for day in days:
+            for meal in days[day]:
+                meals.append(days[day][meal])
+        return meals
